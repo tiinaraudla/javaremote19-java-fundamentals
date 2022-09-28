38c9bade77726d497d0906deedd915ad9f91fc6b
tiinavilu <tiinavilu73@gmail.com>
2022-09-28 18:20:12 +0300

    homework done

--- src/OperatorHomework.java
+++ src/OperatorHomework.java
@@ -1,5 +1,20 @@
 public class OperatorHomework {
     public static void main(String[] args) {
-        System.out.println();
+        int d = 5;
+        int e = 10;
+        int a = 15;
+        int i = 2;
+        int c = d * e;
+        System.out.println(c);
+
+        int y = (c / d) * e - a + i;
+        /*
+        1. y = (50 / 5) * e - a + i;
+        2. y = 10 * 10 - a + i;
+        3. y = 100 - 15 + i;
+        4. y = 85 + 2;
+        5. y = 87
+        */
+        System.out.println(y);
     }
 }
